a4c4e1e043f955c7fa717a21abb3c0ba196c3ef0
Bill Kremer <bkrems12@gmail.com>
2017-01-16 14:29:06 -0600

    adding colored cubes - completed challenge
    the client request to create squares and keep a running total of those colored squares has been met.

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-var colorTotal = {
+var colorTotal = { // an object with the color count total.
   red: 0,
   yellow: 0,
   green: 0,
   blue: 0,
 };
-var $newBlock = "";
-var nextColor = "";
+var $newBlock = ""; // the string for the div to be appended.
+var nextColor = ""; // the color of the div from the button data.
 
-$(function () {
+$(function () { // document ready
 
   $('.color-button').on('click', function () {
-   nextColor = $(this).data('color');
-console.log(nextColor);
+   nextColor = $(this).data('color'); // retrieves the color data
 
+   $newBlock = $('<div class="color-cube ' + nextColor +'"></div>'); // creates a div string with the color-cube and "nextColor" classes.
 
+   $('.container').append($newBlock); // appends the div
 
+   colorTotal[nextColor]++;  // increases the total count of the color
 
-  });
+   $('body').find('#'+nextColor).text(colorTotal[nextColor]);
+   // updates the total color count in the top table.
+
+  });  // closes the click
 }); // closes document ready
